6b7346a6c047272258060a5b72f761d9f9246d93
xink <zyhh1611054604@gmail.com>
2025-08-04 01:40:48 +0800

    fixed bug

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ RGGame.on("PairedTokenMinted", async (user, token, amountIn, pairedTokenAddress,
   const amountOutStr = formatAmount(amountOut);
   if (token === RG_CONTRACT_ADDRESS && amountInStr > 10000) {
     const message = `
-    Token: RUC\n
-    铸造地址: ${user}\n
-    铸造消耗RG：${amountInStr} USDT\n
-    获得RUC: ${amountOutStr} RUC\n
-    交易哈希：${txHash}
-    `
+———铸造RUC—— [${now()}]
+Token: RUC
+铸造地址: ${user}
+铸造消耗RG：${amountInStr} RG
+获得RUC: ${amountOutStr} RUC
+交易哈希：${txHash} `
     await sendTelegramMessage(escapeMarkdownV2(message));
   }
 })
@@ -53,11 +53,11 @@ RGGame.on("WithdrawRequest", async (user, token, amount, timestamp, event) => {
   const amountInStr = formatAmount(amount);
   if (token === RUC_CONTRACT_ADDRESS && amountInStr >= 100000000) {
     const message = `
-    Token: RUC\n
-    提现地址: ${user}\n
-    提现数量：${amountInStr} USDT\n
-    交易哈希：${txHash}
-    `
+———提现RUC—— [${now()}]
+Token: RUC
+提现地址: ${user}
+提现数量：${amountInStr} USDT
+交易哈希：${txHash}`
     await sendTelegramMessage(escapeMarkdownV2(message));
   }
 })
@@ -77,14 +77,14 @@ RG_RUC_PANCAKE.on('Swap', async (sender, amount0In, amount1In, amount0Out, amoun
 
   if (amount0InFmt >= 100000000){
     const message = `
-    Token: RUC\n
-    卖出地址: ${sender}\n
-    卖出：${amount0InFmt} RUC\n
-    获得: ${amount1OutFmt} RG\n
-    当前价格: ${(amount0InFmt / amount1OutFmt).toFixed(4)} RG\n
-    日内涨幅: --\n
-    交易哈希：${txHash}
-    `
+———卖出RUC—SWAP2—— [${now()}]
+Token: RUC
+卖出地址: ${sender}
+卖出：${amount0InFmt} RUC
+获得: ${amount1OutFmt} RG
+当前价格: ${(amount0InFmt / amount1OutFmt).toFixed(4)} RG
+日内涨幅: --
+交易哈希：${txHash}`
     await sendTelegramMessage(escapeMarkdownV2(message.trim()));
   }
 });
@@ -104,30 +104,31 @@ RG_USDT_PANCAKE.on('Swap', async (sender, amount0In, amount1In, amount0Out, amou
 
   if (amount0InFmt >= 5000){
     const message = `
-    Token: RG\n
-    购买地址: ${sender}\n
-    买入：${amount0InFmt} USDT\n
-    购得: ${amount1OutFmt} RG\n
-    当前价格: ${(amount0InFmt / amount1OutFmt).toFixed(4)} USDT\n
-    日内涨幅: --\n
-    交易哈希：${txHash}
+———买入RG—SWAP1—— [${now()}]\n
+Token: RG\n
+购买地址: ${sender}\n
+买入：${amount0InFmt} USDT\n
+购得: ${amount1OutFmt} RG\n
+当前价格: ${(amount0InFmt / amount1OutFmt).toFixed(4)} USDT\n
+日内涨幅: --\n
+交易哈希：${txHash}
     `
     await sendTelegramMessage(escapeMarkdownV2(message.trim()));
   }
 
   if (amount0OutFmt >= 10000){
     const message = `
-    Token: RG\n
-    卖出地址: ${sender}\n
-    卖出：${amount1InFmt} RG\n
-    获得: ${amount0OutFmt} USDT\n
-    当前价格: ${(amount0OutFmt / amount1InFmt).toFixed(4)} USDT\n
-    日内涨幅: --\n
-    交易哈希：${txHash}
+———卖出RG—SWAP1—— [${now()}]\n
+Token: RG\n
+卖出地址: ${sender}\n
+卖出：${amount1InFmt} RG\n
+获得: ${amount0OutFmt} USDT\n
+当前价格: ${(amount0OutFmt / amount1InFmt).toFixed(4)} USDT\n
+日内涨幅: --\n
+交易哈希：${txHash}
     `
     await sendTelegramMessage(escapeMarkdownV2(message.trim()));
   }
-
 });
 
 
